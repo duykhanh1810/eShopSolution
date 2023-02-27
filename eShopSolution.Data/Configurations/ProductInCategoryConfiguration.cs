149e9e8a4fa6fabefc4fb6cd0f7944d9a1cc4207
duykhanh1810 <duykhanh18102002@gmail.com>
2023-02-27 09:38:40 +0700

    Complete Add Entity, Fluent API, Configuration and Migration

---
+++ eShopSolution.Data/Configurations/ProductInCategoryConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class ProductInCategoryConfiguration : IEntityTypeConfiguration<ProductInCategory>
+    {
+
+        public void Configure(EntityTypeBuilder<ProductInCategory> builder)
+        {
+            builder.HasKey(t => new { t.CategoryId, t.ProductId });
+
+            builder.ToTable("ProductInCategories");
+
+            builder.HasOne(p => p.Product).WithMany(pc => pc.ProductInCategories)
+                .HasForeignKey(pc => pc.ProductId); // 1 product có nhiều categories với khóa ngoại là ProductId
+
+            builder.HasOne(c => c.Category).WithMany(pc => pc.ProductInCategories)
+                .HasForeignKey(pc => pc.CategoryId);
+        }
+    }
+}
---
+++ eShopSolution.Data/Entities/ProductInCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Entities
+{
+    public class ProductInCategory
+    {
+        public int ProductId { get; set; } //Post
+        public Product Product { get; set; } //thuộc tính liên kết - Tag
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
