c8a83f01a58af7296d9e5172f1f12c97798df9be
duykhanh1810 <duykhanh18102002@gmail.com>
2023-03-16 20:08:47 +0700

    ex21: Api for login and register with jwt

---
+++ eShopSolution.BackendApi/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using eShopSolution.Application.System.Users;
+using eShopSolution.ViewModels.System.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace eShopSolution.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("authenticate")]
+        [AllowAnonymous] //để không cần đăng nhập cũng có thể load được cái này
+        public async Task<IActionResult> Authenticate([FromForm] LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var resultToken = await _userService.Authencate(request);
+            if (string.IsNullOrEmpty(resultToken))
+            {
+                return BadRequest("Username or password is in correct");
+            }
+            return Ok(new { token = resultToken }); //trả về 1 token
+        }
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromForm] RegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _userService.Register(request);
+            if (!result)
+            {
+                return BadRequest("Register unsuccessful");
+            }
+            return Ok();
+        }
+    }
+}
