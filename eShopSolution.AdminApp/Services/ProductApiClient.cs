c84ee4e80a02b382c90c3986e8d8ed2d2180d805
duykhanh1810 <duykhanh18102002@gmail.com>
2023-03-24 23:47:59 +0700

    ex37: create product with upload file

--- eShopSolution.AdminApp/Services/ProductApiClient.cs
+++ eShopSolution.AdminApp/Services/ProductApiClient.cs
@@ -1,10 +1,12 @@
-﻿using eShopSolution.ViewModels.Catalog.Products;
+﻿using eShopSolution.Utilities.Constants;
+using eShopSolution.ViewModels.Catalog.Products;
 using eShopSolution.ViewModels.Common;
 using eShopSolution.ViewModels.System.Users;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -20,6 +22,9 @@ namespace eShopSolution.AdminApp.Services
         public ProductApiClient(IHttpClientFactory httpClientFactory, IConfiguration configuration,
             IHttpContextAccessor httpContextAccessor) : base(httpClientFactory, configuration, httpContextAccessor)
         {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<PagedResult<ProductVm>> GetPagings(GetManageProductPagingRequest request)
@@ -30,6 +35,52 @@ namespace eShopSolution.AdminApp.Services
                 $"&keyword={request.Keyword}&languageId={request.LanguageId}");
 
             return data;
+        }
+
+        //37.Create product
+        public async Task<bool> CreateProduct(ProductCreateRequest request)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var session = _httpContextAccessor.HttpContext.Session.GetString(SystemConstants.AppSettings.Token);
+
+            var languageId = _httpContextAccessor.HttpContext.Session.GetString(SystemConstants.AppSettings.DefaultLanguageId);
+
+            client.BaseAddress = new Uri(_configuration[SystemConstants.AppSettings.BaseAddress]);
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", session);
+
+            var requestContent = new MultipartFormDataContent();
+
+            //Chuyển Thumpnail image ra dạng binary content
+            if (request.ThumbnailImage != null)
+            {
+                byte[] data;
+                using (var br = new BinaryReader(request.ThumbnailImage.OpenReadStream()))
+                {
+                    data = br.ReadBytes((int)request.ThumbnailImage.OpenReadStream().Length);
+                }
+                ByteArrayContent bytes = new ByteArrayContent(data);
+                requestContent.Add(bytes, "thumbnails", request.ThumbnailImage.FileName);
+            }
+
+            //Add các kiểu dữ liệu còn lại vào theo dạng binary content
+            //(ở ProductCreateRequest có bao nhiêu thì add hết vào)
+
+            requestContent.Add(new StringContent(request.Price.ToString()), "price");
+            requestContent.Add(new StringContent(request.OriginalPrice.ToString()), "originalPrice");
+            requestContent.Add(new StringContent(request.Stock.ToString()), "stock");
+            requestContent.Add(new StringContent(request.Name.ToString()), "name");
+            requestContent.Add(new StringContent(request.Description.ToString()), "description");
+            requestContent.Add(new StringContent(request.Details.ToString()), "details");
+            requestContent.Add(new StringContent(request.SeoDescription.ToString()), "seoDescription");
+            requestContent.Add(new StringContent(request.SeoTitle.ToString()), "seoTitle");
+            requestContent.Add(new StringContent(request.SeoAlias.ToString()), "seoAlias");
+            requestContent.Add(new StringContent(languageId), "languageId");
+
+            //Post dữ liệu đi
+            var response = await client.PostAsync($"/api/products", requestContent);
+            return response.IsSuccessStatusCode;
         }
     }
 }
