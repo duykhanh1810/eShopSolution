ea517aa4b86274a492346054d9a34fa6807c74e7
duykhanh1810 <duykhanh18102002@gmail.com>
2023-04-06 00:36:29 +0700

    ex54: checkout order product

--- eShopSolution.WebApp/Controllers/CartController.cs
+++ eShopSolution.WebApp/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using eShopSolution.ApiIntegration;
 using eShopSolution.Utilities.Constants;
+using eShopSolution.ViewModels.Sales;
 using eShopSolution.WebApp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -83,7 +84,6 @@ namespace eShopSolution.WebApp.Controllers
 
 		public IActionResult UpdateCart(int id, int quantity)
 		{
-
 			//Kiểm tra session
 			var session = HttpContext.Session.GetString(SystemConstants.CartSession);
 			List<CartItemViewModel> currentCart = new List<CartItemViewModel>();
@@ -108,6 +108,51 @@ namespace eShopSolution.WebApp.Controllers
 
 			HttpContext.Session.SetString(SystemConstants.CartSession, JsonConvert.SerializeObject(currentCart));
 			return Ok(currentCart); //trả về tổng current cart
+		}
+
+		public IActionResult Checkout()
+		{
+			return View(GetCheckoutViewModel());
+		}
+
+		[HttpPost]
+		public IActionResult Checkout(CheckoutViewModel request)
+		{
+			var model = GetCheckoutViewModel();
+			var orderDetails = new List<OrderDetailVm>();
+			foreach (var item in model.CartItems)
+			{
+				orderDetails.Add(new OrderDetailVm()
+				{
+					ProductId = item.ProductId,
+					Quantity = item.Quantity
+				});
+			}
+			var checkoutRequest = new CheckoutRequest()
+			{
+				Address = request.CheckoutModel.Address,
+				Name = request.CheckoutModel.Name,
+				Email = request.CheckoutModel.Email,
+				PhoneNumber = request.CheckoutModel.PhoneNumber,
+				OrderDetails = orderDetails
+			};
+			//TODO: Add to API
+			TempData["SuccessMsg"] = "Order puschased successful";
+			return View(model);
+		}
+
+		private CheckoutViewModel GetCheckoutViewModel()
+		{
+			var session = HttpContext.Session.GetString(SystemConstants.CartSession);
+			List<CartItemViewModel> currentCart = new List<CartItemViewModel>();
+			if (session != null)
+				currentCart = JsonConvert.DeserializeObject<List<CartItemViewModel>>(session);
+			var checkoutVm = new CheckoutViewModel()
+			{
+				CartItems = currentCart,
+				CheckoutModel = new CheckoutRequest()
+			};
+			return checkoutVm;
 		}
 	}
 }
