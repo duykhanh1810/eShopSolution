c30869049d2448bfd4a0503bdc754424428d5ca6
duykhanh1810 <duykhanh18102002@gmail.com>
2023-04-05 20:40:22 +0700

    ex53: update cart and fix Add to cart bug

--- eShopSolution.WebApp/Controllers/CartController.cs
+++ eShopSolution.WebApp/Controllers/CartController.cs
@@ -24,7 +24,7 @@ namespace eShopSolution.WebApp.Controllers
             return View();
         }
 
-        //[HttpPost("{id}/{languageId}")]
+
         public async Task<IActionResult> AddToCart(int id,string languageId)
         {
             var product = await _productApiClient.GetById(id,languageId); //lấy ra id product theo language
@@ -40,28 +40,33 @@ namespace eShopSolution.WebApp.Controllers
 
 
             //check xem sản phẩm đã có trong giỏ hàng trước đó chưa
-            int quantity = 1;
-            if(currentCart.Any(x=>x.ProductId==id))
-            {
-                quantity = quantity + currentCart.First(x => x.ProductId == id).Quantity + 1;
-
+            int quantity=1;
+            if (currentCart.Exists(x=>x.ProductId==id))
+            {   
+                foreach (var item in currentCart)
+                {
+                    if(item.ProductId == product.Id)
+                    {
+                        item.Quantity += quantity;
+                    }
+                }
                 //nếu đã có sản phẩm đó trong giỏ hàng mà add thêm vào thì sẽ tự cộng thêm 1 trong quantity
             }
-
-            var cartItem = new CartItemViewModel()
+            else //nếu không có thì add thêm sản phẩm được bấm vào
             {
-                ProductId = id,
-                Description = product.Description,
-                Image = product.ThumbnailImage,
-                Name = product.Name,
-				Price = product.Price,
-				Quantity = quantity
-            };
+				var cartItem = new CartItemViewModel()
+				{
+					ProductId = id,
+					Description = product.Description,
+					Image = product.ThumbnailImage,
+					Name = product.Name,
+					Price = product.Price,
+					Quantity = quantity
+				};
+				currentCart.Add(cartItem);
+			}
 
-            //if (currentCart == null) currentCart = new List<CartItemViewModel>();
-            currentCart.Add(cartItem);
-
-            HttpContext.Session.SetString(SystemConstants.CartSession, JsonConvert.SerializeObject(currentCart));
+			HttpContext.Session.SetString(SystemConstants.CartSession, JsonConvert.SerializeObject(currentCart));
             return Ok(currentCart); //trả về tổng current cart
         }
 
@@ -73,6 +78,36 @@ namespace eShopSolution.WebApp.Controllers
 			if (session != null)
 				currentCart = JsonConvert.DeserializeObject<List<CartItemViewModel>>(session);
 			return Ok(currentCart);
+		}
+
+
+		public IActionResult UpdateCart(int id, int quantity)
+		{
+
+			//Kiểm tra session
+			var session = HttpContext.Session.GetString(SystemConstants.CartSession);
+			List<CartItemViewModel> currentCart = new List<CartItemViewModel>();
+			if (session != null)
+			{
+				//lấy ra cartSession            
+				currentCart = JsonConvert.DeserializeObject<List<CartItemViewModel>>(session);
+			}
+
+            foreach (var item in currentCart)
+            {
+                if (item.ProductId == id)
+                {
+                    if (quantity == 0)
+                    {
+						currentCart.Remove(item);
+                        break;
+					} //nếu ta cho quantity về 0 thì tự động xóa
+					item.Quantity = quantity;
+                }
+			}
+
+			HttpContext.Session.SetString(SystemConstants.CartSession, JsonConvert.SerializeObject(currentCart));
+			return Ok(currentCart); //trả về tổng current cart
 		}
 	}
 }
